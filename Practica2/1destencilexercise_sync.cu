8e1cca5036ffcc83d5f8cec54e4e47d12b210f6b
Alejandro Cabana <alejandro.cabana@estudiante.uam.es>
2020-10-30 00:38:14 +0100

    Stencil

--- Practica2/1destencilexercise_sync.cu
+++ Practica2/1destencilexercise_sync.cu
@@ -14,31 +14,23 @@
   }
 
 __global__ void stencil_1d(int *in, int *out){
-	// __shared__ int temp[/* WHAT SIZE? */];
-	// int gindex = threadIdx.x + (blockIdx.x * blockDim.x) + RADIUS;
-	// int lindex = threadIdx.x + RADIUS;
-
-	// // Read input elements into shared memory
-	// temp[lindex] = in[gindex];
-	// if (threadIdx.x < RADIUS){
-	// 	temp[lindex - RADIUS] = in[gindex - RADIUS];
-	// 	temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
-	// }
-
-	// // Apply the stencil
-	// int result = 0;
-	// for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
-	// 	result += temp[lindex + offset];
-
-	// // Store the result
-	// out[gindex-RADIUS] = result;
-
+	__shared__ int temp[BLOCK_SIZE+2*RADIUS];
 	int gindex = threadIdx.x + (blockIdx.x * blockDim.x) + RADIUS;
+	int lindex = threadIdx.x + RADIUS;
+
+	// Read input elements into shared memory
+	temp[lindex] = in[gindex];
+	if (threadIdx.x < RADIUS){
+		temp[lindex - RADIUS] = in[gindex - RADIUS];
+		temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
+	}
+
+	__syncthreads();
 
 	// Apply the stencil
 	int result = 0;
 	for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
-		result += in[gindex + offset];
+		result += temp[lindex + offset];
 
 	// Store the result
 	out[gindex-RADIUS] = result;
